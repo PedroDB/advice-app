43c20416511c8133339450542e8d9dc6f00a7e3a
PedroDB <casualfury117@gmail.com>
2022-03-04 19:44:01 -0600

    Advice App Finish Result

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react'
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const API_URL = 'https://api.adviceslip.com/advice';
+
+const App = () => {
+
+  const [advice, setAdv] = useState('Searching...');
+
+  const adviceFetch = async() => {
+    const response = await axios(API_URL);
+    const {advice} = response.data.slip;
+
+    setAdv(advice);
+  }
+
+  useEffect(() => {
+    adviceFetch();
+  
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className ='card'>
+        <h1 className='heading'>{advice}</h1>
+        <button className='button' onClick={()=>{adviceFetch()}}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
